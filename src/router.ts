c8bdf6830d01180ffdb62472e60062c6001857fd
Núcleo de Robótica do Cesmac <roboticacesmac@gmail.com>
2023-11-06 11:14:32 -0300

    Reativada rota para testar a API

--- src/router.ts
+++ src/router.ts
@@ -10,6 +10,6 @@ router.use('/', express.static('../web_conciliarepossivel/build'));
 // A rota recebe os parâmetros request e response (back-end)
 router.post('/api/agendar-reuniao', autenticacaoMiddleware.verificarToken, conciliarController.agendarReuniao);
 router.post('/api/enviar-email', autenticacaoMiddleware.verificarToken, conciliarController.enviarEmail);
-// router.get('/', (request: Request, response: Response) => response.status(200).send("A API está funcionando"));
+router.get('/api', (request: Request, response: Response) => response.status(200).send("A API está funcionando"));
 
 export default router;
