d316b6480533c46d48bf7f51fc28b70a46648bdc
Núcleo de Robótica do Cesmac <roboticacesmac@gmail.com>
2023-11-06 10:43:33 -0300

    Adicionado /back-end/ às rotas do back-end

--- src/router.ts
+++ src/router.ts
@@ -8,8 +8,8 @@ const router = express.Router();
 router.use('/', express.static('../web_conciliarepossivel/build'));
 
 // A rota recebe os parâmetros request e response (back-end)
-router.post('/agendar-reuniao', autenticacaoMiddleware.verificarToken, conciliarController.agendarReuniao);
-router.post('/enviar-email', autenticacaoMiddleware.verificarToken, conciliarController.enviarEmail);
+router.post('/back-end/agendar-reuniao', autenticacaoMiddleware.verificarToken, conciliarController.agendarReuniao);
+router.post('/back-end/enviar-email', autenticacaoMiddleware.verificarToken, conciliarController.enviarEmail);
 // router.get('/', (request: Request, response: Response) => response.status(200).send("A API está funcionando"));
 
 export default router;
